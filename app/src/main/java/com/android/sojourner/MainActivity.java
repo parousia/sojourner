f71c35a1e21fb50da0ff7c71339762cd8ee12aaa
ohjoseph <josephoh.kr@gmail.com>
2016-07-15 12:17:43 -0700

    Begin TabLayout initialization

--- app/src/main/java/com/android/sojourner/MainActivity.java
+++ app/src/main/java/com/android/sojourner/MainActivity.java
@@ -1,14 +1,18 @@
 package com.android.sojourner;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find TabLayout
     }
 }
