056c2ef7009e08f8944283f4b3b252b165c2c19e
ohjoseph <josephoh.kr@gmail.com>
2016-07-22 16:43:43 -0700

    Add AllTreksActivity

---
+++ app/src/main/java/com/android/sojourner/AllTreksActivity.java
@@ -0,0 +1,17 @@
+package com.android.sojourner;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Joseph on 7/22/16.
+ */
+
+public class AllTreksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
